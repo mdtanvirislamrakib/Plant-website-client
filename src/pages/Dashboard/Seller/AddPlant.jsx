2e644fac5d762eb587e6a9da90a9c86b5a6db307
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-06-23 18:52:41 +0600

    upload image in imgbb in addPlant

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,13 +1,36 @@
 import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { imageUpload } from '../../../API/Utils';
 
 const AddPlant = () => {
 
-  
+  const handleFormSubmit = async(e) => {
+    e.preventDefault()
+    const form = e.target;
+    const name = form?.name?.value;
+    const category = form?.category?.value;
+    const description = form?.description?.value;
+    const price = form?.price?.value;
+    const quantity = form?.quantity?.value;
+    const image = form?.image?.files[0];
+
+    const imageUrl = await imageUpload(image)
+    const plantData = {
+      name, 
+      category, 
+      description, 
+      price, 
+      quantity, 
+      image: imageUrl
+    }
+
+    console.table(plantData)
+
+  }
 
   return (
     <div>
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm handleFormSubmit = {handleFormSubmit} />
     </div>
   )
 }
